117fa4d11230857b28c4ab59611b8ef0eec586c7
BroofTerr <lukaitis.rytis@gmail.com>
2021-12-17 16:25:15 +0200

    -Loads the full image of the taken picture instead of a thumbnail

--- app/src/main/java/com/example/shoplist/PictureFragment.java
+++ app/src/main/java/com/example/shoplist/PictureFragment.java
@@ -5,28 +5,43 @@ import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.SimpleDateFormat;
+
+import android.net.Uri;
+import java.net.URI;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.location.DetectedActivity;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 
 
 public class PictureFragment extends Fragment
@@ -40,11 +55,23 @@ public class PictureFragment extends Fragment
     private SQLiteDatabase db;
     Bitmap theImage;
 
+    ImageView iv;
+    String currentPhotoPath;
+    ContentValues values;
+    Uri imageUri;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_pictures,container,false);
+
+        iv = view.findViewById(R.id.imageView);
+        if (theImage != null)
+        {
+            iv.setImageBitmap(theImage);
+        }
+
 
         text = view.findViewById(R.id.saveImageText);
         text1 = view.findViewById(R.id.ViewImageText);
@@ -61,8 +88,15 @@ public class PictureFragment extends Fragment
                 }
                 else
                 {
-                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                    values = new ContentValues();
+                    values.put(MediaStore.Images.Media.TITLE, "New Picture");
+                    values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+                    imageUri = getContext().getContentResolver().insert(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    startActivityForResult(intent, CAMERA_REQUEST);
+
                 }
             }
         });
@@ -71,10 +105,8 @@ public class PictureFragment extends Fragment
             @Override
             public void onClick(View v)
             {
-                //((PictureActivity) getActivity()).loadFragment(new PictureStoreDatabaseFragment(), true);
                 Intent i = new Intent(getActivity(), PictureActivity.class);
-                ((PictureActivity) getActivity()).startActivity(i);
-
+                startActivity(i);
             }
         });
         return view;
@@ -97,8 +129,6 @@ public class PictureFragment extends Fragment
 
         ByteArrayOutputStream os = new ByteArrayOutputStream();
 
-        bitmap.compress(Bitmap.CompressFormat.JPEG,100, os);
-
         byte[] imageArr = os.toByteArray();
         return Base64.encodeToString(imageArr, Base64.URL_SAFE);
     }
@@ -108,9 +138,16 @@ public class PictureFragment extends Fragment
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
         {
-            theImage = (Bitmap) data.getExtras().get("data");
+            try {
+                theImage = (Bitmap) MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            iv.setImageBitmap(theImage);
             photo=getEncodedString(theImage);
             setDataToDataBase();
+
         }
     }
 
